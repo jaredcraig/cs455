1eab90535f43356d29ed14c70754f5fe5624ac4b
Jared <jaredmcraig@gmail.com>
2015-10-15 09:59:44 -0600

    finish program 2

--- program2/src/model.cpp
+++ program2/src/model.cpp
@@ -6,95 +6,108 @@
  */
 #include "model.h"
 
-#define POINTS_PER_VERTEX 3
+//-----------------------------------------------------------------------------
+void Model::parseFaceLine(string line, vector<int> &face_line) {
+	line[0] = ' ';
+	string parsed_line;
+	string el;
+	istringstream ss(line);
+
+	while (!ss.eof()) {
+		ss >> el;
+		parsed_line += el;
+		parsed_line += '/';
+	}
+
+	ss.clear();
+	ss.str(parsed_line);
+
+	while (getline(ss, el, '/')) {
+		face_line.push_back(atoi(el.c_str()));
+	}
+}
 
 //-----------------------------------------------------------------------------
-bool Model::openFile(string filename, ifstream& file) {
-	file.open(filename.c_str());
+void Model::parseV(string &line) {
+	line[0] = ' ';
+	char c = line.c_str()[1];
+
+	if (c == 't') {
+		line[1] = ' ';
+		glm::vec2 uv;
+		sscanf(line.c_str(), "%f %f", &uv.x, &uv.y);
+		temp_uvs.push_back(uv);
+	} else if (c == 'n') {
+		glm::vec3 normal;
+		line[1] = ' ';
+		sscanf(line.c_str(), "%f %f %f", &normal.x, &normal.y, &normal.z);
+		temp_normals.push_back(normal);
+	} else {
+		glm::vec3 vertex;
+		sscanf(line.c_str(), "%f %f %f ", &vertex.x, &vertex.y, &vertex.z);
+		temp_vertices.push_back(vertex);
+	}
+}
+
+//-----------------------------------------------------------------------------
+void Model::parseF(string &line) {
+
+	vector<int> faces;
+	parseFaceLine(line, faces);
+
+	vertex_indices.push_back(faces[0]);
+	vertex_indices.push_back(faces[3]);
+	vertex_indices.push_back(faces[6]);
+
+	uv_indices.push_back(faces[1]);
+	uv_indices.push_back(faces[4]);
+	uv_indices.push_back(faces[7]);
+
+	normal_indices.push_back(faces[2]);
+	normal_indices.push_back(faces[5]);
+	normal_indices.push_back(faces[8]);
+}
+
+void Model::buildTriangles() {
+	for (unsigned int i = 0; i < vertex_indices.size(); i++) {
+
+		// Get the indices of its attributes
+		unsigned int vertexIndex = vertex_indices[i];
+		unsigned int uvIndex = uv_indices[i];
+		unsigned int normalIndex = normal_indices[i];
+
+		// Get the attributes thanks to the index
+		glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+		glm::vec2 uv = temp_uvs[uvIndex - 1];
+		glm::vec3 normal = temp_normals[normalIndex - 1];
+
+		// Put the attributes in buffers
+		vertices.push_back(vertex);
+		uvs.push_back(uv);
+		normals.push_back(normal);
+
+	}
+}
+//-----------------------------------------------------------------------------
+void Model::loadObj() {
+	string line;
+	ifstream file("box.obj");
+
 	if (file.is_open()) {
-		file.seekg(0, ios::end);
-		file.seekg(0, ios::beg);
-		return true;
+		while (!file.eof()) {
+			getline(file, line);
+			switch (line.c_str()[0]) {
+			case 'v':
+				parseV(line);
+				break;
+			case 'f':
+				parseF(line);
+				break;
+			}
+		}
+		buildTriangles();
+		file.close();
 	} else {
 		cout << "Unable to open file";
 	}
-	return false;
 }
-
-//-----------------------------------------------------------------------------
-bool Model::parseV(istringstream &iss) {
-	iss.clear();
-	float x, y, z;
-	iss >> x >> y >> z;
-	if (iss.fail()) {
-		cout << "ERROR: could not read in vertex" << endl;
-		return false;
-	}
-
-	points.push_back(x);
-	points.push_back(y);
-	points.push_back(z);
-
-	return true;
-}
-
-//-----------------------------------------------------------------------------
-bool Model::parseF(istringstream &iss) {
-	iss.clear();
-
-	int x, y, z;
-	iss >> x >> y >> z;
-	if (iss.fail()) {
-		cout << "ERROR: could not read in point" << endl;
-		return false;
-	}
-
-	x -= 1;
-	y -= 1;
-	z -= 1;
-
-	triangles.push_back(points.at(3 * x));
-	triangles.push_back(points.at(3 * y));
-	triangles.push_back(points.at(3 * z));
-
-	return true;
-}
-
-//-----------------------------------------------------------------------------
-bool Model::load() {
-	string line;
-	string word;
-	istringstream iss;
-	ifstream file;
-
-	if (!openFile("rock.obj", file))
-		return false;
-
-	while (!file.eof()) {
-		getline(file, line);
-		iss.clear();
-		iss.str(line);
-		iss >> word;
-
-		// vertices
-		if (word.c_str()[0] == 'v') {
-			if (!parseV(iss)) {
-				return false;
-			}
-		}
-
-		// faces
-		if (line.c_str()[0] == 'f') {
-			if (!parseF(iss)) {
-				cout << "ERROR: Parse 'f'" << endl;
-				return false;
-			}
-		}
-	}
-	file.close();
-	cout << "VERTICES: " << triangles.size() << endl;
-
-	return true;
-}
-
-//-----------------------------------------------------------------------------
--- program2/src/model.h
+++ program2/src/model.h
@@ -4,32 +4,23 @@
  *  Created on: Sep 16, 2015
  *      Author: jared
  */
+#include <GL/glew.h>
+#include <GL/glut.h>
+
 #include <sstream>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <stdlib.h>
-#include <GL/glut.h>
-#include <cstdio>
-#include <cmath>
 #include <SOIL/SOIL.h>
+#include <glm/glm.hpp>
 
 using namespace std;
 
-//-----------------------------------------------------------------------------
-class Model {
-public:
-
-	bool load();
-	void draw();
-	bool parseV(istringstream &iss);
-	bool parseF(istringstream &iss);
-	bool openFile(string filename, ifstream&);
-
-	vector<float> points;
-	vector<float> triangles;
-	float g_rotation;
-};
+#define POINTS_PER_VERTEX 3
+#define TOTAL_FLOATS_IN_TRIANGLE 9
+#define TOTAL_FLOATS_IN_QUAD 12
 
 //-----------------------------------------------------------------------------
 typedef struct {
@@ -41,3 +32,22 @@ typedef struct {
 	float z_far;
 } glutWindow;
 
+//-----------------------------------------------------------------------------
+class Model {
+public:
+
+	vector<glm::vec3> temp_vertices;
+	vector<glm::vec2> temp_uvs;
+	vector<glm::vec3> temp_normals;
+	vector<glm::vec3> vertices;
+	vector<glm::vec2> uvs;
+	vector<glm::vec3> normals;
+
+	vector<int> vertex_indices, uv_indices, normal_indices;
+
+	void parseFaceLine(string line, vector<int> &face_line);
+	void parseV(string &line);
+	void parseF(string &line);
+	void buildTriangles();
+	void loadObj();
+};
